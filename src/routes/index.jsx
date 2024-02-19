9d8aa10af7a11120dd0c8d184209618c3f00f061
mustapha jamil <mustaphajay2@gmail.com>
2024-02-19 21:33:13 +0100

    changed the color

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -7,7 +7,7 @@ export default function IndexPage() {
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div className="mx-auto max-w-2xl sm:py-6 ">
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            <h1 className="text-4xl bg-blue-700 font-bold tracking-tight text-gray-900 sm:text-6xl">
               Welcome to THE Conference
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
